4fbf401473738af51e3414e96c547b3c36d8ec63
Ekta3006 <57767742+Ekta3006@users.noreply.github.com>
2022-06-14 23:04:28 +0530

    Update missing and repeating number.cpp

--- missing and repeating number.cpp
+++ missing and repeating number.cpp
@@ -20,3 +20,24 @@ pair<int,int> missingAndRepeating(vector<int> &arr, int n)
     p.second=s;
     return p;
 }
+
+//better approach
+#include <bits/stdc++.h> 
+
+pair<int,int> missingAndRepeating(vector<int> &arr, int n)
+{
+	// Write your code here 
+	 pair<int,int> ans={0,0};
+    if(n==0)
+        return ans;
+    long long int total=(n*(n+1))/2;
+    long long int sqtotal=(n*(n+1)*((2*n)+1))/6;
+    for(int i=0;i<n;i++){
+        total-=arr[i];
+        sqtotal-=(arr[i]*arr[i]);
+    }
+    ans.first=((sqtotal/total)+total)/2;
+    ans.second=ans.first-total;
+    return ans;
+}
+
